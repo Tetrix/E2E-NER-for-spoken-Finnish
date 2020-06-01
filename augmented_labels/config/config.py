1070a569b28e2ff81076a31a9a1a8219a6eeaa5c
Porjazovski Dejan <porjazd1@t40511-da036.org.aalto.fi>
2020-06-01 16:41:33 +0300

    config

---
+++ augmented_labels/config/config.py
@@ -0,0 +1,14 @@
+encoder_layers = 5
+decoder_layers = 1
+
+encoder_hidden_size = 512
+
+encoder_lr_rate = 0.0001
+decoder_lr_rate = 0.0003
+
+embedding_dim_chars = 300
+
+batch_size = 64
+num_epochs = 50
+MAX_LENGTH = 300
+skip_training = False
